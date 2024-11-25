60130c7571d0d62bb250f686f87bbc6a070e1b98
Pallat Anchaleechamaikorn <n668091@AR084667321N2.local>
2024-11-25 09:54:10 +0700

    HA example

---
+++ ha/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	redis "github.com/redis/go-redis/v9"
+)
+
+func main() {
+	masterSet()
+	masterGet()
+	slave()
+}
+
+func masterSet() {
+	ctx := context.Background()
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379", // use default Addr
+		Password: "",               // no password set
+		DB:       0,                // use default DB
+	})
+	defer rdb.Close()
+
+	if _, err := rdb.Ping(ctx).Result(); err != nil {
+		log.Fatal(err)
+	}
+
+	val := "222"
+	if err := rdb.Set(context.Background(), "key1", val, 0).Err(); err != nil {
+		log.Panicf("set %v not success: %s\n", val, err)
+	}
+
+	fmt.Printf("set %q to master successfully\n", val)
+}
+
+func masterGet() {
+	ctx := context.Background()
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379", // use default Addr
+		Password: "",               // no password set
+		DB:       0,                // use default DB
+	})
+	defer rdb.Close()
+
+	if _, err := rdb.Ping(ctx).Result(); err != nil {
+		log.Fatal(err)
+	}
+
+	cmd := rdb.Get(context.Background(), "key1")
+	fmt.Printf("latest value from master: %v\n", cmd.Val())
+}
+
+func slave() {
+	ctx := context.Background()
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6479", // use default Addr
+		Password: "",               // no password set
+		DB:       0,                // use default DB
+	})
+	defer rdb.Close()
+
+	if _, err := rdb.Ping(ctx).Result(); err != nil {
+		log.Fatal(err)
+	}
+
+	cmd := rdb.Get(context.Background(), "key1")
+	fmt.Printf("latest value from slave: %v\n", cmd.Val())
+}
